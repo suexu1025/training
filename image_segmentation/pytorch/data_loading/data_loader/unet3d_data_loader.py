8d32330a803810e5e968c4aa9c3c860c9b0bfc7d
Qinwen Xu <suexu1025@gmail.com>
2022-12-02 22:05:55 +0000

    update tensorboard

--- image_segmentation/pytorch/data_loading/data_loader/unet3d_data_loader.py
+++ image_segmentation/pytorch/data_loading/data_loader/unet3d_data_loader.py
@@ -18,7 +18,7 @@ def list_files_with_pattern(path, files_pattern):
 
 
 def load_data(path, files_pattern):
-    data = sorted(glob.gfile.glob((os.path.join(path, files_pattern))))
+    data = sorted(glob.glob((os.path.join(path, files_pattern))))
     assert len(data) > 0, f"Found no data at {path}"
     return data
 
