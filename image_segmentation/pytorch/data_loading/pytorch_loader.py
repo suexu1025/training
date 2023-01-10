90c360b931ece89487cac7f2ae6ea8796c8a57be
Qinwen Xu <suexu1025@gmail.com>
2023-01-10 23:00:37 +0000

    add simple loader

--- image_segmentation/pytorch/data_loading/pytorch_loader.py
+++ image_segmentation/pytorch/data_loading/pytorch_loader.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 import scipy.ndimage
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset,DataLoader
 from torchvision import transforms
 import gcsfs
 fs = gcsfs.GCSFileSystem()
@@ -135,23 +135,64 @@ class GaussianNoise:
         return data
 
 
+# class PytTrain(Dataset):
+#     def __init__(self, images, labels, **kwargs):
+#         self.images, self.labels = images, labels
+#         self.train_transforms = get_train_transforms()
+#         patch_size, oversampling = kwargs["patch_size"], kwargs["oversampling"]
+#         self.patch_size = patch_size
+#         self.rand_crop = RandBalancedCrop(patch_size=patch_size, oversampling=oversampling)
+
+#     def __len__(self):
+#         return len(self.images)
+
+#     def __getitem__(self, idx):
+#         with fs.open(self.images[idx], 'rb') as f, fs.open(self.labels[idx], 'rb') as g:
+#             data = {"image": np.load(f), "label": np.load(g)}
+#         data = self.rand_crop(data)
+#         data = self.train_transforms(data)
+#         data["image"]=np.transpose(data["image"],(1,2,3,0))
+#         data["label"]=np.transpose(data["label"],(1,2,3,0))        
+#         return data["image"], data["label"]
+
 class PytTrain(Dataset):
-    def __init__(self, images, labels, **kwargs):
-        self.images, self.labels = images, labels
-        self.train_transforms = get_train_transforms()
-        patch_size, oversampling = kwargs["patch_size"], kwargs["oversampling"]
-        self.patch_size = patch_size
-        self.rand_crop = RandBalancedCrop(patch_size=patch_size, oversampling=oversampling)
+    def __init__(
+        self,
+        images, labels, 
+        **kwargs)
+    ): 
+        channels_in=4,
+        channels_out=4,
+        shape=(128, 128, 128),
+        device="cpu",
+        layout="NCDHW",
+        scalar=True,
+        shape = tuple(shape)
+        x_shape = (channels_in,) + shape if layout == "NCDHW" else shape + (channels_in,)
+        self.x = torch.rand(
+            (32, *x_shape), dtype=torch.float32, device=device, requires_grad=False
+        )
+        if scalar:
+            self.y = torch.randint(
+                low=0,
+                high=channels_out - 1,
+                size=(32, *shape),
+                dtype=torch.int32,
+                device=device,
+                requires_grad=False,
+            )
+            self.y = torch.unsqueeze(self.y, dim=1 if layout == "NCDHW" else -1)
+        else:
+            y_shape = (channels_out,) + shape if layout == "NCDHW" else shape + (channels_out,)
+            self.y = torch.rand(
+                (32, *y_shape), dtype=torch.float32, device=device, requires_grad=False
+            )
 
     def __len__(self):
-        return len(self.images)
+        return 64
 
     def __getitem__(self, idx):
-        with fs.open(self.images[idx], 'rb') as f, fs.open(self.labels[idx], 'rb') as g:
-            data = {"image": np.load(f), "label": np.load(g)}
-        data = self.rand_crop(data)
-        data = self.train_transforms(data)
-        return data["image"], data["label"]
+        return self.x[idx % 32], self.y[idx % 32]
 
 class PytVal(Dataset):
     def __init__(self, images, labels):
@@ -163,4 +204,78 @@ class PytVal(Dataset):
     def __getitem__(self, idx):
         with fs.open(self.images[idx], 'rb') as f, fs.open(self.labels[idx], 'rb') as g:
             return np.load(f), np.load(g)
+import os
+import tensorflow as tf
+def load_data(path, files_pattern):
+    data = sorted(tf.io.gfile.glob((os.path.join(path, files_pattern))))
+    assert len(data) > 0, f"Found no data at {path}"
+    return data
 
+def split_eval_data(x_val, y_val, num_shards, shard_id):
+    x = [a.tolist() for a in np.array_split(x_val, num_shards)]
+    y = [a.tolist() for a in np.array_split(y_val, num_shards)]
+    return x[shard_id], y[shard_id]
+
+def get_data_split(path: str, num_shards: int, shard_id: int, use_brats: bool, foldidx: int):
+    if use_brats:
+        listfile = "brats_evaluation_cases_{}.txt".format(foldidx)
+    else:
+        listfile = "evaluation_cases.txt"
+
+    with open(listfile, "r") as f:
+        val_cases_list = f.readlines()
+    val_cases_list = [case.rstrip("\n") for case in val_cases_list]
+    if "tfrecord" in path:
+        imgs = load_data(path, "*.tfrecord")
+        imgs_train, lbls_train, imgs_val, lbls_val = [], [], [], []
+        for case_img in imgs:
+            if case_img.split("_")[-1].split('.')[0] in val_cases_list:
+                imgs_val.append(case_img)
+            else:
+                imgs_train.append(case_img)
+        #mllog_event(key="train_samples", value=len(imgs_train), sync=False)
+        #mllog_event(key="eval_samples", value=len(imgs_val), sync=False)
+        imgs_val, lbls_val = split_eval_data(imgs_val, [], num_shards, shard_id)
+        return imgs_train, imgs_val, lbls_train, lbls_val
+
+    imgs = load_data(path, "*_x.npy")
+    lbls = load_data(path, "*_y.npy")
+    assert len(imgs) == len(lbls), f"Found {len(imgs)} volumes but {len(lbls)} corresponding masks"
+    imgs_train, lbls_train, imgs_val, lbls_val = [], [], [], []
+    for (case_img, case_lbl) in zip(imgs, lbls):
+        if case_img.split("_")[-2] in val_cases_list:
+            imgs_val.append(case_img)
+            lbls_val.append(case_lbl)
+        else:
+            imgs_train.append(case_img)
+            lbls_train.append(case_lbl)
+    #mllog_event(key="train_samples", value=len(imgs_train), sync=False)
+    #mllog_event(key="eval_samples", value=len(imgs_val), sync=False)
+    return imgs_train, imgs_val, lbls_train, lbls_val
+if __name__ == "__main__":
+    path="gs://mlperf-dataset/data/2021_Brats_np/11_3d"
+
+    x_train, x_val, y_train, y_val = get_data_split(
+        path, 4, shard_id=0, use_brats=True, foldidx = 0
+    )
+
+    train_dataset = PytTrain(x_train, y_train, patch_size=[128,128,128], oversampling=0.4)
+    
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=2,
+        shuffle=False,
+        sampler=None,
+        num_workers=4,
+        pin_memory=False,
+        drop_last=True,
+    )
+
+    for image in train_dataset:
+        print(image[0].shape)
+        print(image[1].shape)
+
+    val_dataset = TFVal(x_val)
+    for image in val_dataset:
+        print(image[0].shape)
+        print(image[1].shape)
