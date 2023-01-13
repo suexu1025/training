c4070cef7203b578d5cd6f211e50989f66c9b733
Qinwen Xu <suexu1025@gmail.com>
2023-01-13 01:51:06 +0000

    remove dummy loader

--- image_segmentation/pytorch/data_loading/pytorch_loader.py
+++ image_segmentation/pytorch/data_loading/pytorch_loader.py
@@ -157,44 +157,44 @@ class PytTrain(Dataset):
         data["label"]=np.transpose(data["label"],(1,2,3,0))        
         return data["image"], data["label"]
 
-class PytTrain(Dataset):
-    def __init__(
-        self,
-        images, labels, 
-        **kwargs): 
-        self.images, self.labels = images, labels
-        channels_in=4
-        channels_out=4
-        shape=(128, 128, 128)
-        device="cpu"
-        layout="NCDHW"
-        scalar=True
-        shape = tuple(shape)
-        x_shape = (channels_in,) + shape if layout == "NCDHW" else shape + (channels_in,)
-        self.x = torch.rand(
-            (32, *x_shape), dtype=torch.float32, device=device, requires_grad=False
-        )
-        if scalar:
-            self.y = torch.randint(
-                low=0,
-                high=channels_out - 1,
-                size=(32, *shape),
-                dtype=torch.int32,
-                device=device,
-                requires_grad=False,
-            )
-            self.y = torch.unsqueeze(self.y, dim=1 if layout == "NCDHW" else -1)
-        else:
-            y_shape = (channels_out,) + shape if layout == "NCDHW" else shape + (channels_out,)
-            self.y = torch.rand(
-                (32, *y_shape), dtype=torch.float32, device=device, requires_grad=False
-            )
+# class PytTrain(Dataset):
+#     def __init__(
+#         self,
+#         images, labels, 
+#         **kwargs): 
+#         self.images, self.labels = images, labels
+#         channels_in=4
+#         channels_out=4
+#         shape=(128, 128, 128)
+#         device="cpu"
+#         layout="NCDHW"
+#         scalar=True
+#         shape = tuple(shape)
+#         x_shape = (channels_in,) + shape if layout == "NCDHW" else shape + (channels_in,)
+#         self.x = torch.rand(
+#             (32, *x_shape), dtype=torch.float32, device=device, requires_grad=False
+#         )
+#         if scalar:
+#             self.y = torch.randint(
+#                 low=0,
+#                 high=channels_out - 1,
+#                 size=(32, *shape),
+#                 dtype=torch.int32,
+#                 device=device,
+#                 requires_grad=False,
+#             )
+#             self.y = torch.unsqueeze(self.y, dim=1 if layout == "NCDHW" else -1)
+#         else:
+#             y_shape = (channels_out,) + shape if layout == "NCDHW" else shape + (channels_out,)
+#             self.y = torch.rand(
+#                 (32, *y_shape), dtype=torch.float32, device=device, requires_grad=False
+#             )
 
-    def __len__(self):
-        return 64
+#     def __len__(self):
+#         return 64
 
-    def __getitem__(self, idx):
-        return self.x[idx % 32], self.y[idx % 32]
+#     def __getitem__(self, idx):
+#         return self.x[idx % 32], self.y[idx % 32]
 
 class PytVal(Dataset):
     def __init__(self, images, labels):
